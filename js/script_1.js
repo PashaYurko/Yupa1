9261127e1fe5ab6af3c28790e386c3abc455ef30
Pasha_Yurko <pa.yurko@gmail.com>
2020-06-01 23:16:49 +0300

    пройдено 3-ю практику

--- js/script_1.js
+++ js/script_1.js
@@ -1,17 +1,70 @@
 "use strict";
-const numberOfFilms = prompt("Скільки фільмів ви подивились?", ""),
-    personalVovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };console.log(personalVovieDB);
+  
+// ||
+let numberOfFilms;
 
-    const Q_1 = prompt("Останній переглянутий фільм?", ""),
-          Q_2 = prompt("На скільки його оцінюєш?", ""), 
-          Q_3 = prompt("Останній переглянутий фільм?", ""),
-          Q_4 = prompt("На скільки його оцінюєш?", "");
-          personalVovieDB.movies[Q_1] = Q_2;
-          personalVovieDB.movies[Q_3] = Q_4;
-          
+function start () 
+        {numberOfFilms = +prompt("Скільки ти переглянув фільмів?", "");
+        while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Скільки ти переглянув фільмів?", "");
+    }
+}
+start ();
+const personalVovieDB = {
+    count: numberOfFilms, 
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+    function rememberMyFilms () {
+        for (let i = 0; i < 2; i++) { 
+        const a = prompt("Останній переглянутий фільм?", ""),
+                b = prompt("На скільки його оцінюєш?", "");  
+         if (a !=null && a != "" && b != null && b != "" && a.length <=50 ) {
+           personalVovieDB.movies[a] = b;console.log("done");
+        } else {i--; console.log("Error");}
+        }
+    }
+    rememberMyFilms ();
+    
+    function detectPersonalLevel (){ 
+    if (personalVovieDB.count <= 10) {
+        console.log("Переглянув доволі мало фільмфі");
+    } else if (personalVovieDB.count > 10 && personalVovieDB.count <=30 ){
+        console.log("Ви класичний глядач");
+    } else if (personalVovieDB.count >= 30) {
+        console.log("Ви кіноман !!!");
+    } else {console.log("Виникла помилка");}}
+    detectPersonalLevel();
+
+
+
+        
+    
+            //   for (let i = 0; i > 1; i++){
+            //   if  (numberOfFilms == null || numberOfFilms == "" || numberOfFilms == String || 
+            //     numberOfFilms == Object) 
+            //     {console.log("1"); i++;}
+            // else {i++; console.log("2");}
+     
+            
+function showMyDB (hidden){
+    if (!hidden){
+        console.log(personalVovieDB); 
+        }     
+}
+showMyDB (personalVovieDB.privat);
+
+           
+          function writeYouGenres () {
+              for (let i = 1; i <= 3; i++){
+                personalVovieDB.genres[i-1] = prompt(`Ваш коханий жанр під номером? ${i}`);  
+              }
+          }  
+          writeYouGenres ();
+
+           
+                
+            
+    
